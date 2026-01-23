12a76126bf4cf776390d780430be7749f707d321
Elton Whybrow <eltonwhybrow@Eltons-MacBook-Pro.local>
2026-01-23 12:48:40 +0000

    derp5

--- wp-content/themes/generatepress-child-theme/custom-footer.php
+++ wp-content/themes/generatepress-child-theme/custom-footer.php
@@ -79,7 +79,8 @@
                 <li>Chance to win a FREE T-shirt every month</li>
 
             </ul>
-            <?php echo do_shortcode('[contact-form-7 id="36580a5" title="Subscribe"]'); ?>
+            <?php //echo do_shortcode('[contact-form-7 id="36580a5" title="Subscribe"]'); 
+            ?>
         </div>
     </div>
 
@@ -98,7 +99,7 @@
                 <li>Lock in early access before spots fill up</li>
 
             </ul>
-            <?php echo do_shortcode('[contact-form-7 id="6173ea3" title="FREE_Site_Widesign"]');
+            <?php //echo do_shortcode('[contact-form-7 id="6173ea3" title="FREE_Site_Widesign"]');
             ?>
         </div>
     </div>
