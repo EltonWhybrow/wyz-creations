c1716d42b788751b9d7d87b9e14b393a3177dc17
Elton Whybrow <eltonwhybrow@Eltons-MacBook-Pro.local>
2026-05-06 08:48:47 +0100

    up best sellers shown

--- wp-content/themes/generatepress-child-theme/parts/best-sellers.php
+++ wp-content/themes/generatepress-child-theme/parts/best-sellers.php
@@ -45,7 +45,7 @@ $trusted_groups = array('trusted_1', 'trusted_2', 'trusted_3', 'trusted_4');
                         <?php
                         $args = [
                             'post_type' => 'product',
-                            'posts_per_page' => 16,
+                            'posts_per_page' => 21,
                             'tax_query' => [
                                 [
                                     'taxonomy' => 'product_cat',
